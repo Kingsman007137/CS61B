12bcd1398b71d960774bbc6c276ed629d8300ef1
pan <3398530682@qq.com>
2023-01-11 11:30:33 +0800

    second submit

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,12 +10,10 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        if (head.rest == null) {
+        while (head != null) {
             head.first += c;
-            return;
+            head = head.rest;
         }
-        head.first += c;
-        addConstant(head.rest, c);
     }
 
     /**
